f29d4d93ed7fa984dce6bbb0a32c78b2a8b092e6
loveCamilleTW <yoyopetergod@gmail.com>
2023-05-03 21:11:20 +0800

    Feat: add basic HTML

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import ICON_DICE from './images/icon-dice.svg';
+import DIVIDER_DESKTOP from './images/pattern-divider-desktop.svg';
+// import DIVIDER_MOBILE from './images/pattern-divider-mobile.svg';
+
 function App() {
   return (
-    <>
+    <main>
+      <div className="card">
+        <h1>ADVICE # 1</h1>
+        <p>
+          quote
+        </p>
+        <Divider />
+        <DiceButton />
+      </div>
+    </main>
+  )
+}
 
-    </>
+function Divider() {
+  return (
+    <img src={DIVIDER_DESKTOP} />
+  )
+}
+
+function DiceButton() {
+  return (
+    <button>
+      <img src={ICON_DICE} alt="icon dice" />
+    </button>
   )
 }
 
